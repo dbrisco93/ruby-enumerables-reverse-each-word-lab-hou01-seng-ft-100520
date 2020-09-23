a63db4b950511286504090008dcb96a13f8676a9
Dominique Brisco <dbrisco93@gmail.com>
2020-09-23 04:32:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(string)
-  array = string.
-  
-  array.each do |element|
-    puts array
-  end
+  array = string.split(" ")
+  updated_array = []  
+  updated_array << array
+puts updated_array
 end
