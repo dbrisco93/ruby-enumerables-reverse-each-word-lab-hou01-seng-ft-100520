def6106f32415d4be83eabe6aff43e9c16dc6383
Dominique Brisco <dbrisco93@gmail.com>
2020-09-23 04:50:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   updated_array = []  
   updated_array << array.each do |element|
-    element.reverse
+    element.reverse!
   end
   updated_array.join(" ")
 end
